5ca4975327117bfabfcd79b11489194c5a170557
cristianUS <c.u.s21193@gmail.com>
2021-09-22 12:36:55 -0500

    Fin Seccion 8

---
+++ helpers/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (uid) => {
+    return new Promise ((resolve, reject) => {
+        const payload = {uid};
+
+        jwt.sign(payload, process.env.JWT_KEY, {
+            expiresIn: '24h'
+        }, (err, token) => {
+            if(err){
+                //no se creo token
+                reject('No se pudo generar JWT');
+            }else{
+                //se creo token
+                resolve( token );
+            }
+        });
+    
+    });
+}
+
+
+module.exports = {
+    generarJWT
+}
